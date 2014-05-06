19eef53b568102625f72605810b00bfdbd881f93
Matthias Bolte <matthias@tinkerforge.com>
2014-05-06 18:59:38 +0200

    image: Check for root in prepare script and install more tools

--- image/prepare-host.sh
+++ image/prepare-host.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-# run as root
-
 set -e
 
-echo -e "\nInfo: Installing tools\n"
+. ./utilities.sh
 
-apt-get install qemu-user-static multistrap git-core
+ROOT_UID="0"
+
+# Check if running as root
+if [ "$UID" -ne "$ROOT_UID" ]
+then
+    report_error "You must be root to execute the script"
+    exit 1
+fi
+
+report_info "Installing tools"
+
+apt-get install build-essential qemu-user-static multistrap git-core u-boot-tools
 
 exit 0
