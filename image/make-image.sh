a11305532acc7e1e9e5b436f6c9c4da204e0546c
Ishraq Ibne Ashraf <ishraq@ishraq-desktop>
2014-05-08 00:39:06 +0200

    image: Fixing root check

--- image/make-image.sh
+++ image/make-image.sh
@@ -1,13 +1,11 @@
 #! /bin/bash -exu
 
-UID="$(id -u)"
-
 . ./utilities.sh
 
 ROOT_UID="0"
 
 # Check if running as root
-if [ "$UID" -ne "$ROOT_UID" ]
+if [ "$(id -u)" -ne "$ROOT_UID" ]
 then
     report_error "You must be root to execute the script"
     exit 1
