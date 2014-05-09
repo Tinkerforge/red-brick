710844032b090e1e3cc4aac5e5d56189caf09aa9
Matthias Bolte <matthias@tinkerforge.com>
2014-05-09 12:00:56 +0200

    image: Use patch series files instead of manually specifying patches

--- image/patch-source.sh
+++ image/patch-source.sh
@@ -15,32 +15,15 @@ if [ ! -f "$CONFIG_DIR/image_$1.conf" ]; then
     report_error "No such image configuration"
     exit 1
 fi
-. $CONFIG_DIR/image_$1.conf
 
-# Check U-Boot patch
-if [ ! -e $PATCHES_DIR/u-boot/$UBOOT_PATCH ]
-then
-    report_error "U-Boot patch not found"
-    exit 1
-fi
+CONFIG_NAME=$1
+
+. $CONFIG_DIR/image_$CONFIG_NAME.conf
 
 # Check kernel config
 if [ ! -e $PATCHES_DIR/kernel/$KERNEL_CONFIG ]
 then
     report_error "Kernel config not found"
-    exit 1
-fi
-
-# Check kernel patch
-if [ ! -e $PATCHES_DIR/kernel/$KERNEL_I2C_PATCH ]
-then
-    report_error "Kernel I2C patch not found"
-    exit 1
-fi
-
-if [ ! -e $PATCHES_DIR/kernel/$KERNEL_HCD_AXP_PATCH ]
-then
-    report_error "Kernel HCD_AXP patch not found"
     exit 1
 fi
 
@@ -63,15 +46,14 @@ export PATH=$TOOLS_DIR/$TC_DIR_NAME/bin:$PATH
 
 # Patching U-Boot
 pushd $UBOOT_SRC_DIR > /dev/null
-patch -p1 -r - -i $PATCHES_DIR/u-boot/$UBOOT_PATCH
+xargs -d '\n' -I {} -a $PATCHES_DIR/u-boot/image_$CONFIG_NAME.series patch -p1 -r - -i $PATCHES_DIR/u-boot/{}
 popd > /dev/null
 
 # Patching and configureing the kernel and kernel modules
 pushd $KERNEL_SRC_DIR > /dev/null
 cp $PATCHES_DIR/kernel/$KERNEL_CONFIG ./arch/arm/configs
 make ARCH=arm CROSS_COMPILE=$TC_PREFIX $KERNEL_CONFIG
-patch -p0 -r - -i $PATCHES_DIR/kernel/$KERNEL_I2C_PATCH
-patch -p1 -r - -i $PATCHES_DIR/kernel/$KERNEL_HCD_AXP_PATCH
+xargs -d '\n' -I {} -a $PATCHES_DIR/kernel/image_$CONFIG_NAME.series patch -p1 -r - -i $PATCHES_DIR/kernel/{}
 popd > /dev/null
 
 report_info "Process finished"
