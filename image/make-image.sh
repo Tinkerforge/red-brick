8bc1bab8953e98027a9524fc680430966c0b105d
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-05-26 14:08:01 +0200

    image: Removing unused commented code line and fixing rsync switches

--- image/make-image.sh
+++ image/make-image.sh
@@ -122,10 +122,9 @@ else
     set -e
 fi
 mount $loop_dev_p1 $MOUNT_DIR
-$ADVCP_CMD -garp $ROOTFS_DIR/* $MOUNT_DIR/
-#cp -arp $ROOTFS_DIR/* $MOUNT_DIR/
-rsync -arp $KERNEL_SRC_DIR/$KERNEL_MOD_DIR_NAME/lib/modules $MOUNT_DIR/lib/
-rsync -arp $KERNEL_SRC_DIR/$KERNEL_MOD_DIR_NAME/lib/firmware $MOUNT_DIR/lib/
+$ADVCP_CMD -gr $ROOTFS_DIR/* $MOUNT_DIR/
+rsync -a --no-o --no-g $KERNEL_SRC_DIR/$KERNEL_MOD_DIR_NAME/lib/modules $MOUNT_DIR/lib/
+rsync -a --no-o --no-g $KERNEL_SRC_DIR/$KERNEL_MOD_DIR_NAME/lib/firmware $MOUNT_DIR/lib/
 umount $loop_dev_p1
 
 # Releasing loop device
