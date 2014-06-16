282c71f1489a5acdc24f7d4143ca2e494d1dadd2
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-06-16 17:17:41 +0200

    image: Feature doc generator

--- image/generate-feature-doc.py
+++ image/generate-feature-doc.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    from os import path
+    from sys import argv
+    
+    if len(argv) != 2:
+        print "\nError: Too many or too few parameters (provide image configuration name)\n"
+        exit(1)
+        
+    # Defining paths
+    SCRIPT_RUN_DIR = path.dirname(path.abspath(__file__))
+    BUILD_DIR      = SCRIPT_RUN_DIR+"/build"
+    CONFIG_DIR     = SCRIPT_RUN_DIR+"/config"
+    PATCHES_DIR    = SCRIPT_RUN_DIR+"/patches"
+    
+    if not path.isdir(BUILD_DIR) or not path.isdir(BUILD_DIR) or \
+       not path.isdir(BUILD_DIR):
+        print "\nError: Wrong directory structure\n"
+        exit(1)
+    if not path.exists(CONFIG_DIR + "/image_" + argv[1] + ".conf"):
+        print "\nError: No such configuration\n"
+        exit(1)
