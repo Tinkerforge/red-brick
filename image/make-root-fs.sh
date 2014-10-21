4b17d0d89e022142210e1646ad656a4dbe59647b
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-10-21 11:38:38 +0200

    image: AWK like tool with python support. Will be used for parsing wireless scan results

--- image/make-root-fs.sh
+++ image/make-root-fs.sh
@@ -381,6 +381,7 @@ export LC_ALL=C LANGUAGE=C LANG=C LC_CTYPE=$LOCALE
 easy_install --upgrade pip
 # GROUP-START:python
 pip install pycrypto
+pip install pawk
 # GROUP-END:python
 pip install --upgrade netifaces
 EOF
