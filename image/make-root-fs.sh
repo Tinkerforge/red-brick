05fb8daf2c6b4602d0118e0eff320080c7122080
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2015-01-08 15:54:27 +0100

    image: Proper control of X server enabling brickv to manage it.

--- image/make-root-fs.sh
+++ image/make-root-fs.sh
@@ -754,6 +754,13 @@ export LC_ALL=C LANGUAGE=C LANG=C LC_CTYPE=$LOCALE
 update-rc.d -f dnsmasq remove
 EOF
 
+# Enabling X server in RED Brick way
+report_info "Enabling X server in RED Brick way"
+chroot $ROOTFS_DIR<<EOF
+export LC_ALL=C LANGUAGE=C LANG=C LC_CTYPE=$LOCALE
+touch /etc/tf_x_enabled
+EOF
+
 # Remove apache init script dependency of DNS server
 report_info "Remove apache init script dependency of DNS server"
 chroot $ROOTFS_DIR<<EOF
--- image/patches/root-fs/full/etc/profile.d/startx.sh
+++ image/patches/root-fs/full/etc/profile.d/startx.sh
@@ -1,4 +1,4 @@
 # if logging into tty2 (which will autologin), run startx
-if [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty2 ]; then
+if [ -f /etc/tf_x_enabled ] && [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty2 ]; then
     exec startx
 fi
