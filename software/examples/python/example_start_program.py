24b3473cb3d112b4e4691bfd27facc9a2c6704f9
Matthias Bolte <matthias@tinkerforge.com>
2015-05-29 13:33:09 +0200

    Add a Python example

---
+++ software/examples/python/example_start_program.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HOST = "localhost"
+PORT = 4223
+UID = "3dfgRc" # Change to your UID
+PROGRAM = "test"
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.brick_red import RED
+
+def check_error(error_code, *args):
+    if error_code != RED.ERROR_CODE_SUCCESS:
+        print("RED Brick error occurred: {0}".format(error_code))
+        exit(1)
+
+    if len(args) == 1:
+        return args[0]
+
+    return args
+
+if __name__ == '__main__':
+    ipcon = IPConnection() # Create IP connection
+    red = RED(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    # Create session and get program list
+    session_id = check_error(*red.create_session(30))
+    program_list_id = check_error(*red.get_programs(session_id))
+    started = False
+
+    # Iterate program list to find the one to start
+    for i in range(check_error(*red.get_list_length(program_list_id))):
+        program_id, _ = check_error(*red.get_list_item(program_list_id, i, session_id))
+
+        # Get program identifer string
+        string_id = check_error(*red.get_program_identifier(program_id, session_id))
+        string_length = check_error(*red.get_string_length(string_id))
+        string_data = ""
+
+        while len(string_data) < string_length:
+            string_data += check_error(*red.get_string_chunk(string_id, len(string_data)))
+
+        check_error(red.release_object(string_id, session_id))
+
+        # Check if this is the program to be started
+        if string_data.decode("utf-8") == PROGRAM:
+            check_error(red.start_program(program_id))
+            print('Started RED Brick program: {0}'.format(PROGRAM))
+            started = True
+
+        check_error(red.release_object(program_id, session_id))
+
+        if started:
+            break
+
+    check_error(red.release_object(program_list_id, session_id))
+    check_error(red.expire_session(session_id))
+
+    if not started:
+        print('RED Brick program not found: {0}'.format(PROGRAM))
+
+    raw_input('Press key to exit\n') # Use input() in Python 3
+    ipcon.disconnect()
