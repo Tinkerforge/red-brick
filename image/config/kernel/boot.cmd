f40f63a697a3d02e6b19a6c46ae4726523939703
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-06-29 14:03:39 +0200

    image: Cleanup kernel boot script

--- image/config/kernel/boot.cmd
+++ image/config/kernel/boot.cmd
@@ -1,9 +1,13 @@
 load mmc 0:1 0x46000000 boot/vmlinuz
 load mmc 0:1 0x49000000 boot/dt/red-brick.dtb
 
-setenv bootargs_main console=tty1 earlyprintk=serial,ttyS3,115200n8 root=/dev/mmcblk0p1 rw init=/sbin/init quiet splash loglevel=7 panic=5 consoleblank=0
-setenv bootargs_extra hdmi.audio=EDID:0 disp.screen0_output_mode=EDID:800x480p60
-setenv bootargs "${bootargs_main} ${bootargs_extra}"
+setenv arg_console console=tty1
+setenv arg_earlyprintk earlyprintk=serial,ttyS3,115200n8
+setenv arg_rootfs root=/dev/mmcblk0p1 rw
+setenv arg_init init=/sbin/init
+setenv arg_other quiet splash loglevel=7 panic=5 consoleblank=0
+
+setenv bootargs "${arg_console} ${arg_earlyprintk} ${arg_rootfs} ${arg_init} ${arg_other}"
 
 saveenv
 
