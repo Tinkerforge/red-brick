10a9d513e08f90d942d02205bb4297907ec00851
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2017-07-12 11:36:39 +0200

    image: While starting X and desktop environment first switch to tty2

--- image/patches/root-fs/full/etc/profile.d/startx.sh
+++ image/patches/root-fs/full/etc/profile.d/startx.sh
@@ -1,4 +1,5 @@
 # if logging into tty2 (which will autologin), run startx
 if [ -f /etc/tf_x11_enabled ] && [ -z "$DISPLAY" ] && [ $(tty) = /dev/tty2 ]; then
-    exec startx
+    chvt 2
+    exec startx &> /dev/null
 fi
