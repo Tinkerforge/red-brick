cbde483d0275f7c7e810a4ca5169dbc69b837372
Erik Fleckstein <erik@tinkerforge.com>
2020-01-14 13:32:20 +0100

    examples: Use python3 input by default.

--- software/examples/python/example_start_program.py
+++ software/examples/python/example_start_program.py
@@ -67,5 +67,5 @@ if __name__ == '__main__':
     else:
         print('RED Brick program not found: {0}'.format(PROGRAM))
 
-    raw_input('Press key to exit\n') # Use input() in Python 3
+    input("Press key to exit\n") # Use raw_input() in Python 2
     ipcon.disconnect()
