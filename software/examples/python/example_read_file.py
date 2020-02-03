58afc7e2e9b975e2b09e6e7a66b65c46c8ead9fe
Matthias Bolte <matthias@tinkerforge.com>
2020-02-03 16:03:51 +0100

    python: Add read/write files examples

---
+++ software/examples/python/example_read_file.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HOST = 'localhost'
+PORT = 4223
+UID = 'XXYYZZ' # Change XXYYZZ to the UID of your RED Brick
+REMOTE_PATH = '/home/tf/foobar.txt' # Change to your remote path
+LOCAL_PATH = 'foobar.txt' # Change to your local path
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.brick_red import BrickRED
+
+def check_error(error_code, *args):
+    if error_code != 0:
+        print('RED Brick error occurred: {0}'.format(error_code))
+        exit(1)
+
+    if len(args) == 1:
+        return args[0]
+
+    return args
+
+def allocate_string(red, string, session_id):
+    string_id = check_error(*red.allocate_string(len(string), string[:58], session_id))
+
+    for offset in range(58, len(string), 58):
+        check_error(*red.set_string_chunk(string_id, offset, string[offset:offset + 58]))
+
+    return string_id
+
+def read_file(red, remote_path, local_path):
+    # Create session
+    session_id = check_error(*red.create_session(60))
+
+    # Create remote non-executable file for writing as user/group tf
+    remote_path_id = allocate_string(red, remote_path, session_id)
+    remote_file_id = check_error(*red.open_file(remote_path_id,
+                                                BrickRED.FILE_FLAG_READ_ONLY |
+                                                BrickRED.FILE_FLAG_NON_BLOCKING,
+                                                0, 0, 0, session_id))
+
+    check_error(red.release_object(remote_path_id, session_id))
+
+    # Open local file for writing
+    local_file = open(local_path, 'wb')
+
+    # Read remote file and write to local file
+    transferred = 0
+
+    while True:
+        data, length_read = check_error(*red.read_file(remote_file_id, 61))
+        data = data[:length_read]
+
+        if len(data) == 0:
+            break
+
+        local_file.write(bytes(data))
+
+        check_error(red.keep_session_alive(session_id, 30))
+
+        transferred += length_read
+
+    # Close local file
+    local_file.close()
+
+    # Close remote file
+    check_error(red.release_object(remote_file_id, session_id))
+
+    # Expire session
+    check_error(red.expire_session(session_id))
+
+    print('{0} bytes transferred'.format(transferred))
+
+if __name__ == '__main__':
+    ipcon = IPConnection() # Create IP connection
+    red = BrickRED(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    read_file(red, REMOTE_PATH, LOCAL_PATH)
+
+    input("Press key to exit\n") # Use raw_input() in Python 2
+    ipcon.disconnect()
---
+++ software/examples/python/example_write_file.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+HOST = 'localhost'
+PORT = 4223
+UID = 'XXYYZZ' # Change XXYYZZ to the UID of your RED Brick
+LOCAL_PATH = 'foobar.txt' # Change to your local path
+REMOTE_PATH = '/home/tf/foobar.txt' # Change to your remote path
+
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.brick_red import BrickRED
+
+def check_error(error_code, *args):
+    if error_code != 0:
+        print('RED Brick error occurred: {0}'.format(error_code))
+        exit(1)
+
+    if len(args) == 1:
+        return args[0]
+
+    return args
+
+def allocate_string(red, string, session_id):
+    string_id = check_error(*red.allocate_string(len(string), string[:58], session_id))
+
+    for offset in range(58, len(string), 58):
+        check_error(*red.set_string_chunk(string_id, offset, string[offset:offset + 58]))
+
+    return string_id
+
+def write_file(red, local_path, remote_path):
+    # Open local file for reading
+    local_file = open(local_path, 'rb')
+
+    # Create session
+    session_id = check_error(*red.create_session(60))
+
+    # Create remote non-executable file for writing as user/group tf
+    remote_path_id = allocate_string(red, remote_path, session_id)
+    remote_file_id = check_error(*red.open_file(remote_path_id,
+                                                BrickRED.FILE_FLAG_WRITE_ONLY |
+                                                BrickRED.FILE_FLAG_CREATE |
+                                                BrickRED.FILE_FLAG_TRUNCATE |
+                                                BrickRED.FILE_FLAG_NON_BLOCKING,
+                                                0o644, 1000, 1000, session_id))
+
+    check_error(red.release_object(remote_path_id, session_id))
+
+    # Read local file and write to remote file
+    transferred = 0
+
+    while True:
+        data = list(local_file.read(61))
+
+        if len(data) == 0:
+            break
+
+        length_to_write = len(data)
+        data += [0] * (61 - length_to_write)
+        length_written = check_error(*red.write_file(remote_file_id, data, length_to_write))
+
+        if length_written != length_to_write:
+            print('Short write')
+            exit(1)
+
+        check_error(red.keep_session_alive(session_id, 30))
+
+        transferred += length_written
+
+    # Close remote file
+    check_error(red.release_object(remote_file_id, session_id))
+
+    # Close local file
+    local_file.close()
+
+    # Expire session
+    check_error(red.expire_session(session_id))
+
+    print('{0} bytes transferred'.format(transferred))
+
+if __name__ == '__main__':
+    ipcon = IPConnection() # Create IP connection
+    red = BrickRED(UID, ipcon) # Create device object
+
+    ipcon.connect(HOST, PORT) # Connect to brickd
+    # Don't use device before ipcon is connected
+
+    write_file(red, LOCAL_PATH, REMOTE_PATH)
+
+    input("Press key to exit\n") # Use raw_input() in Python 2
+    ipcon.disconnect()
